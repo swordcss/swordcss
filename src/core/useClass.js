c7fc44be5c9e478cdbdfa0c030bc9dc0b8553fff
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-16 09:02:04 -0700

    Some Error Handling

--- src/core/useClass.js
+++ src/core/useClass.js
@@ -1,5 +1,10 @@
 module.exports = (rule, ast) => {
   rule.findDeclarationsByProperty("sw-class", (declaration, index) => {
+    if (
+      declaration.getParam(0) === undefined ||
+      declaration.getParam(0) === null
+    )
+      throw new SyntaxError(`Expected string, got none`);
     rule.removeDeclaration(index);
     ast.findAllDeclarationsBySelectors(`.${declaration.getParam(0)}`, (dec) => {
       if (typeof dec === "string") return;
