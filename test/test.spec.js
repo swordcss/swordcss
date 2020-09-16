3d104aeccfa1e6663bc64c75b2f9648a5c27845a
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-16 08:54:40 -0700

    Prettier Workflow, Unit Tests

---
+++ test/test.spec.js
@@ -0,0 +1,30 @@
+const { expect } = require("chai");
+
+const SwordCSS = require("../src/index");
+
+const sword = SwordCSS();
+
+describe("#SwordCSS", () => {
+  describe(".useClass", () => {
+    it("should compile correctly", () => {
+      expect(
+        sword
+          .compile(".elem{width:100%;height:100%;}#elem{sw-class:elem;}")
+          .replace(/[\r\n ]+/gm, "")
+      ).to.equal(
+        ".elem{width:100%;height:100%;}#elem{height:100%;width:100%;}"
+      );
+    });
+  });
+  describe(".useId", () => {
+    it("should compile correctly", () => {
+      expect(
+        sword
+          .compile(".elem{sw-id:elem;}#elem{width:100%;height:100%;}")
+          .replace(/[\r\n ]+/gm, "")
+      ).to.equal(
+        ".elem{height:100%;width:100%;}#elem{width:100%;height:100%;}"
+      );
+    });
+  });
+});
