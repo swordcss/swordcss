1ff962f605b67629621ffa6c0bec8260e06ae309
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-18 09:37:17 -0700

    Removed unnecessary template strings

--- src/core/useQuery.js
+++ src/core/useQuery.js
@@ -1,7 +1,7 @@
 module.exports = (rule, ast) => {
     rule.findDeclarationsByProperty("sw-query", (declaration, index) => {
       rule.removeDeclaration(index);
-      ast.findAllDeclarationsBySelectors(`${declaration.getParam(0)}`, (dec) => {
+      ast.findAllDeclarationsBySelectors(declaration.getParam(0), (dec) => {
         if (typeof dec === "string") return;
         rule.addDeclaration(dec.property, dec.getParam(0), index);
       });
