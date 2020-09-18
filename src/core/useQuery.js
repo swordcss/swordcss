237eddcfe98a270d331f9989f19af773f41063cd
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-09-18 09:55:26 -0700

    Formatted

--- src/core/useQuery.js
+++ src/core/useQuery.js
@@ -1,10 +1,9 @@
 module.exports = (rule, ast) => {
-    rule.findDeclarationsByProperty("sw-query", (declaration, index) => {
-      rule.removeDeclaration(index);
-      ast.findAllDeclarationsBySelectors(declaration.getParam(0), (dec) => {
-        if (typeof dec === "string") return;
-        rule.addDeclaration(dec.property, dec.getParam(0), index);
-      });
+  rule.findDeclarationsByProperty("sw-query", (declaration, index) => {
+    rule.removeDeclaration(index);
+    ast.findAllDeclarationsBySelectors(declaration.getParam(0), (dec) => {
+      if (typeof dec === "string") return;
+      rule.addDeclaration(dec.property, dec.getParam(0), index);
     });
-  };
-  
+  });
+};
