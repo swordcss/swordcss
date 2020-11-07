0ebe4d52aec11f790ed095ef7f1e49990c7ed20e
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-06 18:15:15 -0800

    refactor: use ? : shorthand for if statement

--- src/core/useConstant.js
+++ src/core/useConstant.js
@@ -4,14 +4,7 @@ const cb = (rule, ast) => {
   rule.findDeclarations((declaration, index) => {
     ast.findAllDeclarations((dec) => {
       if (dec.value.match(declaration.property)) {
-        if (dec.value.match(/ /)) {
-          dec.value = dec.value.replace(
-            new RegExp(declaration.property, "g"),
-            declaration.value
-          );
-        } else {
-          dec.value = declaration.value;
-        }
+        dec.value.match(/ /) ? dec.value = dec.value.replace(new RegExp(declaration.property, "g"), declaration.value) : dec.value = declaration.value;
       }
     });
     rule.removeDeclaration(index);
