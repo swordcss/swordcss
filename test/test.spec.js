75016887cb9aad19e6fff58ee72aca120c33c77a
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-12 15:03:27 -0800

    tests: move useconstant to its own spec file

--- test/test.spec.js
+++ test/test.spec.js
@@ -5,47 +5,6 @@ const SwordCSS = require("../src/index");
 const sword = SwordCSS({ minify: true });
 
 describe("#SwordCSS", () => {
-  describe(".useConstant", () => {
-    it("should compile correctly", () => {
-      expect(
-        sword.compile("@sw-constants{const1:red;}.elem{color:const1;}")
-      ).to.equal(".elem{color:red;}");
-    });
-
-    it("should calculate numerical constants correctly", () => {
-      expect(
-        sword.compile(
-          "@sw-constants{const1:30px;}#elem{width:calc(const1 * 20);}"
-        )
-      ).to.equal("#elem{width:calc(30px * 20);}");
-    });
-
-    it("should calculate numerical constants correctly when they appear multiple times", () => {
-      expect(
-        sword.compile(
-          "@sw-constants{const1:30px;}#elem{width:calc(const1 * 20 + const1);}"
-        )
-      ).to.equal("#elem{width:calc(30px * 20 + 30px);}");
-    });
-
-    it("should replace constants correctly when they're part of a larger expression", () => {
-      expect(
-        sword.compile(
-          "@sw-constants{const1:red;}#elem{border:1px solid const1;}"
-        )
-      ).to.equal("#elem{border:1px solid red;}");
-    });
-
-    // disabled option test
-    it("shouldn't use the constant when option is disabled", () => {
-      expect(
-        SwordCSS({ useConstant: false, minify: true }).compile(
-          "@sw-constants{const1:red;}.elem{color:const1;}"
-        )
-      ).to.equal("@sw-constants{const1:red;}.elem{color:const1;}");
-    });
-  });
-
   describe(".useVariable", () => {
     it("should compile correctly", () => {
       expect(
