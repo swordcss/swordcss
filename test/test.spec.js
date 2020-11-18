c4fb16465d39cbc042547046b0f1030f50879966
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-18 08:54:07 -0800

    chore: formatted

--- test/test.spec.js
+++ test/test.spec.js
@@ -17,7 +17,11 @@ describe("#SwordCSS", () => {
   it("should not set the options it should only create a new instance", () => {
     const sword = SwordCSS({ minify: true });
     const sword2 = sword.setOpts({ minify: false });
-    expect(sword.compile("\n.elem{\n\twidth: 20px;\n}\n")).to.equal(".elem{width:20px;}");
-    expect(sword2.compile("\n.elem{\n\twidth: 20px;\n}\n")).to.not.equal(".elem{width:20px;}");
+    expect(sword.compile("\n.elem{\n\twidth: 20px;\n}\n")).to.equal(
+      ".elem{width:20px;}"
+    );
+    expect(sword2.compile("\n.elem{\n\twidth: 20px;\n}\n")).to.not.equal(
+      ".elem{width:20px;}"
+    );
   });
 });
