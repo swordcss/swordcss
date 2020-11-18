a64e7225f47b5697a7efaa94d47081fca1767134
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-18 08:53:56 -0800

    test: added some more tests for settings opts

--- test/test.spec.js
+++ test/test.spec.js
@@ -14,4 +14,10 @@ describe("#SwordCSS", () => {
       sword.setOpts({ minify: false }).compile("\n.elem{\n\twidth: 20px;\n}\n")
     ).to.not.equal(".elem{width:20px;}");
   });
+  it("should not set the options it should only create a new instance", () => {
+    const sword = SwordCSS({ minify: true });
+    const sword2 = sword.setOpts({ minify: false });
+    expect(sword.compile("\n.elem{\n\twidth: 20px;\n}\n")).to.equal(".elem{width:20px;}");
+    expect(sword2.compile("\n.elem{\n\twidth: 20px;\n}\n")).to.not.equal(".elem{width:20px;}");
+  });
 });
