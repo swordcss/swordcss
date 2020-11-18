6f88ef72899d8c4f83c01b75520dd0f9f03bd106
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-18 08:48:06 -0800

    chore: formatted

--- test/test.spec.js
+++ test/test.spec.js
@@ -10,6 +10,8 @@ describe("#SwordCSS", () => {
   });
   it("should allow you to set options", () => {
     const sword = SwordCSS({ minify: true });
-    expect(sword.setOpts({ minify: false }).compile("\n.elem{\n\twidth: 20px;\n}\n")).to.not.equal(".elem{width:20px;}");
+    expect(
+      sword.setOpts({ minify: false }).compile("\n.elem{\n\twidth: 20px;\n}\n")
+    ).to.not.equal(".elem{width:20px;}");
   });
 });
