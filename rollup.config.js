fe189b7fae3e5ab65ecd82a91fd904360ecc0f74
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2021-01-09 01:25:41 -0800

    chore: formatted

--- rollup.config.js
+++ rollup.config.js
@@ -4,30 +4,27 @@ import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-    input: "src/index.js",
-    output: [
-        {
-            file: "dist/swordcss.cjs",
-            format: "cjs"
-        },
-        {
-            file: "dist/swordcss.min.cjs",
-            format: "cjs",
-            plugins: [terser()]
-        },
-        {
-            file: "dist/swordcss.es.js",
-            format: "es"
-        },
-        {
-            file: "dist/swordcss.es.min.js",
-            format: "es",
-            plugins: [terser()]
-        }
-    ],
-    plugins: [
-        resolve(),
-        commonjs()
-    ],
-    external: ["css", "css-ast-iterations", "css-ast-iterations-pure"]
-}
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/swordcss.cjs",
+      format: "cjs",
+    },
+    {
+      file: "dist/swordcss.min.cjs",
+      format: "cjs",
+      plugins: [terser()],
+    },
+    {
+      file: "dist/swordcss.es.js",
+      format: "es",
+    },
+    {
+      file: "dist/swordcss.es.min.js",
+      format: "es",
+      plugins: [terser()],
+    },
+  ],
+  plugins: [resolve(), commonjs()],
+  external: ["css", "css-ast-iterations", "css-ast-iterations-pure"],
+};
